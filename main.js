026582b61b06f8e78806e109b8b344beacb7446f
SalavatIskhakov <salavat-iskhakov@mail.ru>
2024-03-06 21:30:36 +0300

    show output path

--- main.js
+++ main.js
@@ -9,6 +9,11 @@ function createMainWindow() {
     title: 'Image Resizer',
     width: isDev ? 1000 : 500,
     height: 600,
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: true,
+      preload: path.join(__dirname, 'preload.js')
+    },
   })
 
   if (isDev) {
--- renderer/js/renderer.js
+++ renderer/js/renderer.js
@@ -13,6 +13,14 @@ function loadImage(e) {
     return
   }
 
+  const image = new Image()
+  image.src = URL.createObjectURL(file)
+  image.onload = function () {
+    widthInput.value = this.width
+    heightInput.value = this.height
+    outputPath.innerText = path.join(os.homedir(), 'imageResizer')
+  }
+
   form.style.display = 'block'
   filename.innerText = file.name
 }
