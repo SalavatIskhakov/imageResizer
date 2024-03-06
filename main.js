31f4de9af9127d983769d00d2b3d6d44d3001aad
SalavatIskhakov <salavat-iskhakov@mail.ru>
2024-03-06 20:15:42 +0300

    menu

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const path = require('path')
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu } = require('electron')
 
 const isDev = process.env.NODE_ENV !== 'production'
 const isMac = process.platform === 'darwin'
@@ -21,12 +21,53 @@ function createMainWindow() {
 app.whenReady().then(() => {
   createMainWindow()
 
+  const mainMenu = Menu.buildFromTemplate(menu)
+  Menu.setApplicationMenu(mainMenu)
+
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createMainWindow()
     }
   })
 })
+
+const menu = [
+  ...(isMac
+    ? [
+        {
+          label: app.name,
+          submenu: [
+            {
+              label: 'About',
+            },
+          ],
+        },
+      ]
+    : []),
+  {
+    role: 'fileMenu',
+    // label: 'File',
+    // submenu: [
+    //   {
+    //     label: 'Quit',
+    //     click: () => app.quit(),
+    //     accelerator: 'CommandOrControl+W',
+    //   },
+    // ],
+  },
+  ...(!isMac
+    ? [
+        {
+          label: 'Help',
+          submenu: [
+            {
+              label: 'About',
+            },
+          ],
+        },
+      ]
+    : []),
+]
 
 app.on('window-all-closed', () => {
   if (!isMac) {
